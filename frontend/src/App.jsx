53b13c552a62bac3767ad43ccf24271c05aecd61
Huy Phung <huyphung3103@gmail.com>
2024-07-26 12:11:31 -0400

    add navbar folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import React from 'react'
 import './App.css'
+import NavBar from './components/NavBar/NavBar.jsx'
 
 function App() {
 
   return (
     <div>
       <h1>Music App</h1>
+      <NavBar />
     </div>
   )
 }
