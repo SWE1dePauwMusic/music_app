19b8e1f41c636e60559ebe4e0574c5d33717a711
Huy Phung <huyphung3103@gmail.com>
2024-07-27 17:11:41 -0400

    clean root and add sidebar and navbar together

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div>
-      <h1>Music App</h1>
-      <FontAwesomeIcon icon={faCheck} />
       <NavBar />
       <Sidebar />
     </div>
