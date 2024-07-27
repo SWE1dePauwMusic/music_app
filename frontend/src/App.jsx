dd9f74232d503a6bed4f97a693ebcef050592aa6
Huy Phung <huyphung3103@gmail.com>
2024-07-27 03:06:06 -0400

    add navbar and style button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import './App.css'
 import NavBar from './components/NavBar/index.jsx'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faCheck} from '@fortawesome/free-solid-svg-icons'
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div>
-      <h1>Music App</h1>
-      <FontAwesomeIcon icon={faCheck} />
       <NavBar />
     </div>
   )
--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './index.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAngleLeft, faAngleRight, faBell} from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
+  const [activeCategory, setActiveCategory] = useState('');
   return (
-    <div>
-      
+    <div className='navbar-container'>
+      <div className="navbar-left">
+        <button className='icon-btn'>
+          <FontAwesomeIcon icon={faAngleLeft}/>
+        </button>
+        <button className='icon-btn'>
+          <FontAwesomeIcon icon={faAngleRight}/>
+        </button>
+        <div className="category-buttons">
+          {
+            ['All', 'Music', 'Podcast', 'Audiobooks'].map((category) => (
+              <button
+                key = {category}
+                className = {`category-btn ${activeCategory === category ? 'active' : ''}`}
+                onClick = {() => setActiveCategory(category)}
+              >
+                {category}
+              </button>
+            ))
+          }
+            {/* <button className='category-btn'>All</button>
+            <button className='category-btn'>Music</button>
+            <button className='category-btn'>Podcast</button>
+            <button className='category-btn'>Audiobooks</button> */}
+        </div>
+      </div>
+      <div className="navbar-right">
+        <button className='icon-btn'>
+          <FontAwesomeIcon icon={faBell}/>
+        </button>
+        <button className='icon-btn'>
+          <img src="https://randomuser.me/api/portraits"/>
+        </button>
+      </div>
     </div>
   )
 }
