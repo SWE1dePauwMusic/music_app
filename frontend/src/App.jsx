383fa24f1bed13478b5aae0a98bbf732f93160b9
Huy Phung <huyphung3103@gmail.com>
2024-07-26 23:04:22 -0400

    change navbar inside to index

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
-import NavBar from './components/NavBar/NavBar.jsx'
-
+import NavBar from './components/NavBar/index.jsx'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCheck} from '@fortawesome/free-solid-svg-icons'
 function App() {
-
   return (
     <div>
       <h1>Music App</h1>
+      <FontAwesomeIcon icon={faCheck} />
       <NavBar />
     </div>
   )
