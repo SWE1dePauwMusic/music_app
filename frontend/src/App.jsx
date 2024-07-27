c9862e83d7c1184cccec585cf41dfd1f3b21704f
Huy Phung <huyphung3103@gmail.com>
2024-07-27 17:59:43 -0400

    add router link to navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import './App.css'
-import { Sidebar } from './components/SideBar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/SideBar'
 import NavBar from './components/NavBar/index.jsx'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Sidebar />
-    </div>
+    <Router> 
+      <div>
+        <NavBar />
+          <Routes>
+            <Route path='/' element={ <Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        {/* <Sidebar /> */}
+      </div>
+    </Router>
   )
 }
 
--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,46 +1,19 @@
 import React, {useState} from 'react'
 import './index.css'
+import {Link} from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faAngleLeft, faAngleRight, faBell} from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
-  const [activeCategory, setActiveCategory] = useState('');
   return (
-    <div className='navbar-container'>
-      <div className="navbar-left">
-        <button className='icon-btn'>
-          <FontAwesomeIcon icon={faAngleLeft}/>
-        </button>
-        <button className='icon-btn'>
-          <FontAwesomeIcon icon={faAngleRight}/>
-        </button>
-        <div className="category-buttons">
-          {
-            ['All', 'Music', 'Podcast', 'Audiobooks'].map((category) => (
-              <button
-                key = {category}
-                className = {`category-btn ${activeCategory === category ? 'active' : ''}`}
-                onClick = {() => setActiveCategory(category)}
-              >
-                {category}
-              </button>
-            ))
-          }
-            {/* <button className='category-btn'>All</button>
-            <button className='category-btn'>Music</button>
-            <button className='category-btn'>Podcast</button>
-            <button className='category-btn'>Audiobooks</button> */}
-        </div>
-      </div>
-      <div className="navbar-right">
-        <button className='icon-btn'>
-          <FontAwesomeIcon icon={faBell}/>
-        </button>
-        <button className='icon-btn'>
-          <img src="https://randomuser.me/api/portraits"/>
-        </button>
-      </div>
-    </div>
+    <nav className='navbar-container'>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
   )
 }
 
