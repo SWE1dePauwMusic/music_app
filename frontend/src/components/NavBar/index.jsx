5e5f9c18f62f269266a78a8af00df79722de5aac
Huy Phung <huyphung3103@gmail.com>
2024-07-28 01:58:39 -0400

    complete navbar without animation

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -2,16 +2,35 @@ import React, {useState} from 'react'
 import './index.css'
 import {Link} from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faAngleLeft, faAngleRight, faBell} from '@fortawesome/free-solid-svg-icons'
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import {faUser} from '@fortawesome/free-regular-svg-icons'
 
 const NavBar = () => {
   return (
     <nav className='navbar-container'>
       <ul>
+        <li className='brand-name'><h1>Melify</h1></li>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/services">Services</Link></li>
         <li><Link to="/contact">Contact</Link></li>
+        <li className='nav-right'>
+          <div className="search-container">
+            <input type='text' placeholder='Search' className='search-input'/>
+            <FontAwesomeIcon 
+              icon={faMagnifyingGlass}
+              className='search-icon'
+              onClick={() => console.log('searching...')}
+            />
+          </div>
+          <div className="user-image-container">
+            <FontAwesomeIcon 
+              icon={faUser}
+              className='user-image'
+              onClick={() => console.log('user profile')}
+            />
+          </div>
+        </li>
       </ul>
     </nav>
   )
