cae4442ef5058c904cc2dfa3bcd8154994f692f7
Huy Phung <huyphung3103@gmail.com>
2024-07-28 03:05:30 -0400

    add hover and active link to element

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,30 +1,39 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './index.css'
-import {Link} from 'react-router-dom'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
-import {faUser} from '@fortawesome/free-regular-svg-icons'
+import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
 
 const NavBar = () => {
   return (
     <nav className='navbar-container'>
       <ul>
         <li className='brand-name'><h1>Melify</h1></li>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li>
+          <NavLink
+            to="/"
+            style={({ isActive }) => {
+              return isActive ? { color: 'var(--theme-color)' } : {};
+            }}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li><NavLink to="/about" >About</NavLink></li>
+        <li><NavLink to="/services">Services</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
         <li className='nav-right'>
           <div className="search-container">
-            <input type='text' placeholder='Search' className='search-input'/>
-            <FontAwesomeIcon 
+            <input type='text' placeholder='Search' className='search-input' />
+            <FontAwesomeIcon
               icon={faMagnifyingGlass}
               className='search-icon'
               onClick={() => console.log('searching...')}
             />
           </div>
           <div className="user-image-container">
-            <FontAwesomeIcon 
+            <FontAwesomeIcon
               icon={faUser}
               className='user-image'
               onClick={() => console.log('user profile')}
