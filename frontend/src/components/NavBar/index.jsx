0bd554e064dd69f500641cca09ad4b032d17c694
Huy Phung <huyphung3103@gmail.com>
2024-07-28 23:38:59 -0400

    complete user profile icon and search bar

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import { Nav, Navbar, NavDropdown, Form, FormControl, Button, InputGroup } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Form, FormControl } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons';
@@ -14,29 +14,46 @@ function NavbarComponent() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto" activeKey={location.pathname}>
-        <Nav.Link as={Link} to='/' eventKey="/">Home</Nav.Link>
-        <Nav.Link as={Link} to='/about' eventKey="/about">About</Nav.Link>
-        <Nav.Link as={Link} to="/services" eventKey="/services">Services</Nav.Link>
-        <Nav.Link as={Link} to="/contact" eventKey="/contact">Contact</Nav.Link>
+          <Nav.Link as={Link} to='/' eventKey="/">Home</Nav.Link>
+          <Nav.Link as={Link} to='/about' eventKey="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/services" eventKey="/services">Services</Nav.Link>
+          <Nav.Link as={Link} to="/contact" eventKey="/contact">Contact</Nav.Link>
         </Nav>
-        <div className="search-container">
-          <Form className='d-flex'>
-            <FormControl
-              type="text"
-              placeholder="Search"
-              className="search-input"
-              aria-label="Search"
-            />
-            <FontAwesomeIcon icon={faMagnifyingGlass} className='search-icon' />
-          </Form>
-        </div>
-        <div className="user-image-container">
-          <FontAwesomeIcon
-            icon={faUser}
-            className='user-image'
-            onClick={() => console.log('user profile')}
+        <Form className='d-flex search-container'>
+          <FormControl
+            type="text"
+            placeholder="Search"
+            className="search-input"
+            aria-label="Search"
           />
-        </div>
+          <FontAwesomeIcon 
+            icon={faMagnifyingGlass} 
+            className='search-icon'
+            onClick={() => console.log('search')} 
+          />
+        </Form>
+        <NavDropdown 
+          title={         
+            <div className="user-image-container">
+              <FontAwesomeIcon
+                icon={faUser}
+                className='user-image'
+                noCaret="true"
+                onClick={() => console.log('user profile')}/>
+            </div>
+          }
+          className='toggle-user-dropdown'
+          autoClose="outside"
+          align="end"
+          id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">
+            User Setting
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">
+            Log Out
+          </NavDropdown.Item>
+        </NavDropdown>
       </Navbar.Collapse>
     </Navbar>
   );
