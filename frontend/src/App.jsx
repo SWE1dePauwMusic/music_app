0ad29b9f053ef437f6f5834af0f794145f4eed2c
nguyenhue <huenguyen_2026@depauw.edu>
2024-07-30 01:48:41 -0400

    fix postion of navbar and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Recent from './components/Recent';
-import Playlist from './components/Playlist';
-import Favorite from './components/Favorite';
-import Chat from './pages/Chat';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar/index.jsx'
-import Home from './pages/Home'
-import About from './pages/About'
-import Services from './pages/Services'
-import Contact from './pages/Contact'
-import SidebarContainer from './components/SideBar';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Recent from "./components/Recent";
+import Playlist from "./components/Playlist";
+import Favorite from "./components/Favorite";
+import Chat from "./pages/Chat";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar/index.jsx";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import SidebarContainer from "./components/SideBar";
 function App() {
   return (
-    <Router> 
-      <div className='app-container'>
+    <Router>
+      <div className="app-container">
         <NavBar />
         <div className="content-container">
-<<<<<<< HEAD
-          <SidebarContainer />
-          {/* <main className="main-content">
-=======
-          {/* <SidebarContainer /> */}
+          <div className="sidebar">
+            <SidebarContainer />
+          </div>
           <main className="main-content">
->>>>>>> 79cf0555d6b8b9dc98e85917076e65a8d1954bea
             <Routes>
              
               <Route path='/' element={ <Home />} />
@@ -38,19 +35,14 @@ function App() {
               <Route path='/favourite' element={<Favorite />} />
               <Route path='/chat' element={<Chat />} />
             </Routes>
-<<<<<<< HEAD
-          </main> */}
-          {/* <Footer /> */}
-=======
+            <div className="footer">
+              <Footer />
+            </div>
           </main>
-        <div className="footer">
-          <Footer />
->>>>>>> 79cf0555d6b8b9dc98e85917076e65a8d1954bea
-        </div>
         </div>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
