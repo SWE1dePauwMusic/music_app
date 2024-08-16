a405d849eb63448e563a6a01b9e432429d3b1f2a
Huy Phung <huyphung3103@gmail.com>
2024-08-16 01:51:21 -0400

    add style and hovering for playlist card

--- frontend/src/components/Playlist/index.jsx
+++ frontend/src/components/Playlist/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './index.css'
 import { playlistData } from '../../hardCodeData.js'
+import { Card } from 'react-bootstrap'
 
 const Playlist = () => {
   console.log(playlistData)
   return (
-    <div>
-      Playlist
+    <div className='playlist-component-container'>
+      <Card className='playlist-component-card text-white' >
+        <Card.Img variant="top" src={playlistData.images.url} as="img" width={playlistData.images.width} />
+        <Card.Body>
+          <Card.Title className='mt-'>{playlistData.name}</Card.Title>
+          <Card.Text className='text-white-50'>
+            Playlist • {playlistData.owner.name}
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
